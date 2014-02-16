e6b39367762a8aa88e66f1b770643d29404acfcc
Michaldwadwa <michalskalkowski@gmail.com>
2014-02-16 18:18:23 +0100

    dziala w poziomie

--- Menu/gen/com/example/menu/R.java
+++ Menu/gen/com/example/menu/R.java
@@ -10,6 +10,9 @@ package com.example.menu;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int tlo=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,29 +20,36 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090005;
+        public static final int przycisk_autor=0x7f090002;
+        public static final int przycisk_nowa_gra=0x7f090000;
+        public static final int przycisk_opis=0x7f090001;
+        public static final int przycisk_wyjscie=0x7f090003;
+        public static final int tresc_autor=0x7f090004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int autor=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int autor=0x7f050005;
-        public static final int etykieta_nowa_gra=0x7f050003;
-        public static final int opis=0x7f050004;
-        public static final int tytul=0x7f050002;
-        public static final int wyjscie=0x7f050006;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int autor=0x7f060005;
+        public static final int etykieta_nowa_gra=0x7f060003;
+        public static final int informacje_autor=0x7f060007;
+        public static final int opis=0x7f060004;
+        public static final int tytul=0x7f060002;
+        public static final int wyjscie=0x7f060006;
     }
     public static final class style {
         /** 
@@ -63,10 +73,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
